5dc4f65a1b705829fd6ac7f9a1b4cd4fa859b2a3
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2025-07-16 02:00:05 +0800

    feat: add seeder on model jenis tanaman and kriteria

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password'=> bcrypt('12345678'),
+        ]);
+
+        $this->call([
+            JenisTanamanSeeder::class,
+            KriteriaSeeder::class,
         ]);
     }
 }
--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kriteria;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,51 @@ class KriteriaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $kriterias = array(
+            array(
+                "id" => 3,
+                "nama" => "pH air",
+                "deskripsi" => "pH",
+                "created_at" => "2025-07-15 17:44:30",
+                "updated_at" => "2025-07-15 17:44:30",
+            ),
+            array(
+                "id" => 4,
+                "nama" => "Part Per Million (PPM)",
+                "deskripsi" => "Part Per Million (PPM)",
+                "created_at" => "2025-07-15 17:44:41",
+                "updated_at" => "2025-07-15 17:44:41",
+            ),
+            array(
+                "id" => 5,
+                "nama" => "ketinggian air",
+                "deskripsi" => "ketinggian air",
+                "created_at" => "2025-07-15 17:44:49",
+                "updated_at" => "2025-07-15 17:44:49",
+            ),
+            array(
+                "id" => 6,
+                "nama" => "gejala",
+                "deskripsi" => "gejala",
+                "created_at" => "2025-07-15 17:44:54",
+                "updated_at" => "2025-07-15 17:44:54",
+            ),
+            array(
+                "id" => 7,
+                "nama" => "umur panen",
+                "deskripsi" => "umur panen",
+                "created_at" => "2025-07-15 17:45:03",
+                "updated_at" => "2025-07-15 17:45:03",
+            ),
+            array(
+                "id" => 8,
+                "nama" => "luas lahan",
+                "deskripsi" => "luas lahan",
+                "created_at" => "2025-07-15 17:45:14",
+                "updated_at" => "2025-07-15 17:45:14",
+            ),
+        );
+
+        Kriteria::insert($kriterias);
     }
 }
